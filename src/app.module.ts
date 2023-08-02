ea3705639d4faf4c6a9657bd9ac6c3b1acf94fd6
Manav Kaushal <manavkaushal756@gmail.com>
2023-08-02 14:41:32 +0530

    signup route complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { BookModule } from './book/book.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     BookModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [],
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcryptjs';
+import { Model } from 'mongoose';
+import { SignUpDto } from './dto/signup.dto';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async singUp(singUpDto: SignUpDto): Promise<{ token: string }> {
+    const { name, email, password } = singUpDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
+}
